25dfdfbe2425f3baa1e04246a89aa2a3b0b3f86b
Mariano Jason <mjasonrc@gmail.com>
2023-11-09 12:13:59 +0100

    Added :lipstick: user name

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -40,23 +40,21 @@
             <div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
                 <!-- Profile dropdown -->
                 <div class="relative ml-3" x-data="{open_profile:false}">
-                    <div>
-                        <div> My Name </div>
-                        <button type="button" @click="open_profile = !open_profile" class="relative flex rounded-full bg-gray-800 text-sm hover:outline-none hover:ring-2 hover:ring-white hover:ring-offset-2 hover:ring-offset-gray-800" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
+                    <div class="flex flex-row items-center">
+                        <div class="p-2 text-white hidden sm:block" > {{Auth::user()->name}} </div>
+                        <button type="button" @click="open_profile = !open_profile" 
+                        class="relative flex rounded-full bg-gray-800 text-sm hover:outline-none hover:ring-2 hover:ring-white hover:ring-offset-2 hover:ring-offset-gray-800" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
                             <img class="h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
                         </button>
                     </div>
 
                     <div x-show="open_profile" class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
-                        <!-- Active: "bg-gray-100", Not Active: "" -->
                         <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-0">Your Profile</a>
 
                         <form method="post" action="{{ route('signout') }}">
                             @csrf
                             <button type="submit" class="block px-4 py-2 text-sm text-gray-700" >Sign Out</button>
                         </form>
-
-                        <!-- <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-2">Sign out</a> -->
                     </div>
 
                 </div>
