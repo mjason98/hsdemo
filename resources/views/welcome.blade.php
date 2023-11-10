ea77c1a5f8d32c5e439923606783541c2084fe2c
Mariano Jason <mjasonrc@gmail.com>
2023-11-10 15:58:27 +0100

    Wellcome page first style

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,17 +3,21 @@
         <div class="w-full h-full flex mx-auto">
 
             <div class="flex flex-row w-full">
-                <div class="flex basis-1/2 items-center justify-between bg-cover bg-center invert hidden sm:block" style="background-image:url('{{ asset('imgs/flbg.jpg')}}');">
+                <div class="flex basis-1/2 items-center justify-between bg-cover bg-center hidden sm:block" style="background-image:url('{{ asset('imgs/flbg.jpg')}}');">
                 </div>
 
-                <div class="flex grow min-h-full flex-col justify-center">
+                <div class="flex grow min-h-full flex-col justify-center" style="background-image:url('{{ asset('imgs/bgalt.jpg')}}'); background-size: 500px;">
                     <div class="flex flex-row justify-center">
-                        <div class="flex flex-col items-center">
+                        <div class="flex flex-col items-center gap-20 shadow-xl rounded-xl py-6 bg-white">
                             <x-applogo />
-                            <div class="mt-20 w-80 text-4xl text-center" style="font-family: 'SpringSummer', sans-serif;">
-                                <p> Share the joy of cooking.
-                                <p> Create, discover and connect through the love of food.
+                            <div class="w-80 text-4xl text-center bold" style="font-family: 'Josefin Sans', sans-serif;">
+                                Create and discover through the love of food 
                             </div>
+                            <form action="{{ route('signin.form') }}" method="get">
+                                <button class="relative bg-black rounded-xl text-amber-500 py-2 px-6 border-b-4 border-amber-500 hover:border-blue-400 hover:text-blue-500">
+                                    Signin
+                                </button>
+                            </form>
                         </div>
                     </div>
                 </div>
