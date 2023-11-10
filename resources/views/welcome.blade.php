5cfc465a8f960f0b0ca4a2c70ffc70d1b571d9ed
Mariano Jason <mjasonrc@gmail.com>
2023-11-10 18:30:44 +0100

    Changed :lipstick: welcome style

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,16 +2,16 @@
     <div class="flex items-center justify-center h-screen bg-cover bg-center bg-back">
             
     <!-- Left Column with Image Background -->
-        <div class="w-1/2 h-full bg-cover bg-right invert relative" style="background-image: url('{{ asset('imgs/flbg.jpg')}}'); background-size: 600px;">
+        <div class="w-1/2 h-full bg-cover bg-right invert relative" style="background-image: url('{{ asset('imgs/43030.jpg')}}'); background-size: 2000px;">
         </div>
         <!-- Right Column with Image Background -->
-        <div class="w-1/2 h-full bg-cover bg-left relative" style="background-image: url('{{ asset('imgs/flbg.jpg')}}'); background-size: 600px;">
+        <div class="w-1/2 h-full bbox bg-cover bg-left relative" style="background-image: url('{{ asset('imgs/43030.jpg')}}'); background-size: 2000px;">
         </div>
 
         <!-- Centered Div on Top -->
-        <div class="absolute z-10 bbox flex flex-col items-center gap-24 shadow-xl rounded-xl py-6 bg-white">
+        <div class="absolute z-10 w-96 flex flex-col items-center gap-32 shadow-xl rounded-xl py-6 bg-white">
             <x-applogo />
-            <div class="text-5xl text-center bold" style="font-family: 'Josefin Sans', sans-serif;">
+            <div class="text-5xl w-80 text-center bold" style="font-family: 'Josefin Sans', sans-serif;">
                 Create and discover through the love of food
             </div>
             <form action="{{ route('signin.form') }}" method="get">
@@ -22,10 +22,5 @@
         </div>
     </div>
 
-    <style>
-        .bbox{
-            width: calc(100% - 200px);
-            height: calc(100% - 200px);
-        }
-    </style>
+    
 </x-layout>
