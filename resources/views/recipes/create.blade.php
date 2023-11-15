a0a2837c4c230cb767be72cbd53f7df72a758e6b
Mariano Jason <mjasonrc@gmail.com>
2023-11-15 12:18:25 +0100

    Added basic create form

--- resources/views/recipes/create.blade.php
+++ resources/views/recipes/create.blade.php
@@ -1,13 +1,21 @@
 <x-layout>
-    <div class="flex flex-col p-6 gap-6">
-        <div class="text-gray-800 text-4xl"> title </div>
+    <form class="flex flex-col p-6 gap-6" action="{{route('recipes.store')}}" method="post">
+        @csrf
+        <div class="flex flex-row w-full justify-center items-center">
+            <button type="submit" class="text-xl w-fit h-10 px-4 bg-blue-700 text-white rounded-xl p-2 hover:bg-blue-900">
+                <i class="fa-solid fa-file"></i>
+                Create
+            </button>
+        </div>
+
+        <input value="{{old('title')?? '# Title' }}" type="text" name="title" id="title" class="text-gray-800 text-4xl bg-transparent">
+        
         <!-- <div> Image </div> -->
         <!-- <div> Ingredients </div> -->
         <div class="text-gray-800 text-2xl">
             Instructions
         </div>
-        <div class="text-gray-700 text-lg">
-            intructions 1 2 ...
-        </div>
-    </div>
+
+        <textarea name="instructions" id="instructions" rows="4" class="px-3 text-gray-700 text-lg bg-transparent h-auto">{{old('instructions') ?? 'Seteps ....' }}</textarea>
+    </form>
 </x-layout>
