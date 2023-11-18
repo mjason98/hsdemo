869e28240843c83794885cf534b434a3e2d75184
Mariano Jason <mjasonrc@gmail.com>
2023-11-18 22:46:58 +0100

    Changed email style for new user view

--- resources/views/emails/new_user_notification.blade.php
+++ resources/views/emails/new_user_notification.blade.php
@@ -1,3 +1,25 @@
-<div>
-    new user bitch
-</div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>New User Notification</title>
+</head>
+<body>
+    <h2>New user: {{ $user->name }}!</h2>
+
+    A new food lover has join our comunity :)
+
+    <p>
+        Here are their registration details:
+        <ul>
+            <li><strong>Name:</strong> {{ $user->name }}</li>
+            <li><strong>Email:</strong> {{ $user->email }}</li>
+        </ul>
+    </p>
+
+    We are growing!!!
+    
+    Regards
+</body>
+</html>
