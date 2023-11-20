4fc44c89e77ffef5634304bf86007487d304b6f6
Mariano Jason <mjasonrc@gmail.com>
2023-11-20 18:43:50 +0100

    Added responsivness to discover

--- resources/views/search/index.blade.php
+++ resources/views/search/index.blade.php
@@ -1,6 +1,6 @@
 <x-layout>
     <div class="py-3 px-4 flex flex-col w-full gap-4 items-center" style="height: calc(100vh - 64px);">
-        <form class="flex flex-col sm:flex-row w-70 text-xl sm:h-fit p-4 gap-10 sm:gam-5" action="{{route('search.index')}}" method="get">
+        <form class="flex flex-col sm:flex-row sm:w-70 w-full text-xl sm:h-fit p-4 gap-10 sm:gam-5" action="{{route('search.index')}}" method="get">
             <input class="text-gray-800 text-4xl h-auto resize-none border-2 rounded-xl border-gray-800 p-3 focus:outline-none bg-white" type="text" name="search_string" id="search_string" value="{{old('search_string')??$search_string}}" placeholder="title, ingredients, tags"/>
             
             <x-basic-button>
