2e2ac850ef22e492c8fdf0f052a215171c19acf3
Mariano Jason <mjasonrc@gmail.com>
2023-11-20 17:45:24 +0100

    Added recipes to controller

--- app/Http/Controllers/SearchRecipeController.php
+++ app/Http/Controllers/SearchRecipeController.php
@@ -3,11 +3,26 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Recipes;
 
 class SearchRecipeController extends Controller
 {
     public function index(Request $request)
     {
-        return view('search.index');
+        if ($request->search_string == null) {
+            $recipes = [];
+            $search_string = '';
+        } else {
+            $search_string = $request->search_string;
+
+            $recipes = Recipes::query()
+                ->with(['ingredients', 'tags', 'media'])
+                ->orderBy('created_at', 'desc')
+                ->get();
+        }
+        
+        ray($search_string);
+
+        return view('search.index', compact(['recipes', 'search_string']));
     }
 }
