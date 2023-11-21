31f13e356bda35594569ff06fa0a32f687794044
Mariano Jason <mjasonrc@gmail.com>
2023-11-21 10:01:20 +0100

    Added cache to sudgestions

--- app/Http/Controllers/ExploreController.php
+++ app/Http/Controllers/ExploreController.php
@@ -3,17 +3,20 @@
 namespace App\Http\Controllers;
 
 use App\Models\Recipes;
+use Illuminate\Support\Facades\Cache;
 
 class ExploreController extends Controller
 {
     public function index()
     {
-        $recipes = Recipes::query()
+        $recipes = Cache::remember('explore.recipes', 3600, function(){
+            return Recipes::query()
             ->with(['ingredients', 'tags', 'media'])
             ->where('users_id', '<>', auth()->id())
             ->inRandomOrder()
             ->limit(2)
             ->get();
+        });
 
         return view('explore', compact('recipes'));
     }
--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,6 +1,6 @@
 @auth
 <nav class="bg-gray-900" x-data="{ open_menu:false }">
-    <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+    <div class="mx-2 px-2 sm:px-6 lg:px-8 w-full">
         <div class="relative flex h-16 items-center justify-between">
             <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
                 <!-- Mobile menu button-->
